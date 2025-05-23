d5239b6ba8e9b290bb582065aa2a8e9c7e91ffc3
Alexia-Gomez <alexiaareg_23@alu.uabcs.mx>
2025-05-22 23:40:07 -0700

    Vista clientes

--- MediaVault/src/customClasses/RoundedButton.java
+++ MediaVault/src/customClasses/RoundedButton.java
@@ -1,5 +1,6 @@
 package customClasses;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -10,6 +11,7 @@ import javax.swing.JButton;
 public class RoundedButton extends JButton{
 	
 	private Color color;
+	private Color borderColor= null;
 	private int radius = 0;
 	
 	public Color getColor() {
@@ -30,14 +32,29 @@ public class RoundedButton extends JButton{
 		setForeground(Color.white);
 		setText(string);
 	}	
+
+	
+	public void setBorderColor(Color color) {
+		this.borderColor=color;
+		repaint();
+	}
 	
 	@Override
 	protected void paintComponent(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g.create();
 		
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		
 		g2.setColor(getBackground());
 		g2.fillRoundRect(0,0, getWidth() , getHeight() , radius, radius);
+		
+		if (borderColor != null) {
+            g2.setColor(borderColor);
+            g2.setStroke(new BasicStroke(1f));
+            g2.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, radius, radius);
+        }
+		
 		super.paintComponent(g2);
 	}
+
 }
