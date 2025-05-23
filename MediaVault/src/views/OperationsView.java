2b776536c35995ee711a8c627e96616ae81a402b
Alexia-Gomez <alexiaareg_23@alu.uabcs.mx>
2025-05-22 23:50:31 -0700

    Vista nueva operación

--- MediaVault/src/views/OperationsView.java
+++ MediaVault/src/views/OperationsView.java
@@ -4,23 +4,35 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 import customClasses.Fuentes;
+import customClasses.RoundedButton;
+import customClasses.RoundedJTextField;
 import customClasses.RoundedPanel;
 import customClasses.SideBar;
 
 public class OperationsView {
 
 	Color blue = new Color(24, 130, 234);
+	Color border = new Color(186, 186, 186);
+	Color lightGray = new Color(117, 117, 117);
+	Color field = new Color(250, 250, 250);
 
 	Fuentes tipoFuentes = new Fuentes();
-	Font titles = tipoFuentes.fuente("/fonts/GolosText-Regular.ttf", 16f);
-
+	Font titles = tipoFuentes.fuente("/fonts/GolosText-SemiBold.ttf", 17f);
+	Font btn = tipoFuentes.fuente("/fonts/GolosText-Regular.ttf", 14f);
+	Font txt = tipoFuentes.fuente("/fonts/GolosText-Regular.ttf", 12f);
+	
+	ImageIcon lupa = new ImageIcon(ClientsView.class.getResource("/images/lupa.png"));
+	ImageIcon mas = new ImageIcon(ClientsView.class.getResource("/images/mas.png"));
+	ImageIcon filter = new ImageIcon(ClientsView.class.getResource("/images/filter.png"));
 
 	public OperationsView() {
 
@@ -66,6 +78,99 @@ public class OperationsView {
 		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		titleLabel.setFont(titles);
 		titlePanel.add(titleLabel);
+		
+		RoundedButton newClient = new RoundedButton("Registrar operación");
+		newClient.setIcon(new ImageIcon(((ImageIcon) mas).getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH)));
+		newClient.setBounds(594, 11, 185, 43);
+		newClient.setIconTextGap(10);
+		newClient.setBackground(blue);
+		newClient.setFont(btn);
+		newClient.setRadius(30);
+		centro.add(newClient);
+		
+		//BARRA
+		RoundedPanel barra = new RoundedPanel(30, new Color(255, 255, 255));
+		barra.setBounds(151, 65, 626, 68);
+		barra.setLayout(null);
+		centro.add(barra);
+
+		JLabel lupaIcon = new JLabel("");
+		lupaIcon.setIcon(new ImageIcon(((ImageIcon) lupa).getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH)));
+		lupaIcon.setHorizontalAlignment(SwingConstants.CENTER);
+		lupaIcon.setBounds(20, 20, 30, 30);
+		barra.add(lupaIcon);
+
+		RoundedJTextField searchBar = new RoundedJTextField(20);
+		searchBar.setBounds(65, 20, 348, 30);
+		searchBar.setBackground(field);
+		searchBar.setFont(txt);
+		barra.add(searchBar);
+
+		RoundedButton buscar = new RoundedButton("Buscar");
+		buscar.setBounds(423, 20, 86, 30);
+		buscar.setBackground(blue);
+		buscar.setFont(btn);
+		buscar.setRadius(20);
+		barra.add(buscar);
+
+		RoundedButton filtrar = new RoundedButton("Filtrar");
+		filtrar.setIcon(new ImageIcon(((ImageIcon) filter).getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH)));
+		filtrar.setHorizontalTextPosition(SwingConstants.LEFT);
+		filtrar.setHorizontalAlignment(SwingConstants.CENTER);
+		filtrar.setBounds(515, 20, 86, 30);
+		filtrar.setBackground(Color.white);
+		filtrar.setForeground(Color.black);
+		filtrar.setBorderColor(border);
+		filtrar.setIconTextGap(5);
+		filtrar.setRadius(20);
+		filtrar.setFont(btn);
+		barra.add(filtrar);
+
+
+		//TABLA (HOLDER)
+		//TITLES
+		JPanel tableTitles = new JPanel();
+		tableTitles.setBounds(151, 144, 626, 29);
+		tableTitles.setLayout(new GridLayout(0, 5, 0, 0));
+		centro.add(tableTitles);
+
+		JLabel lblNewLabel_1 = new JLabel("Cliente");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setForeground(lightGray);
+		lblNewLabel_1.setFont(txt);
+		tableTitles.add(lblNewLabel_1);
+
+		JLabel lblNewLabel_2 = new JLabel("Tipo");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setForeground(lightGray);
+		lblNewLabel_2.setFont(txt);
+		tableTitles.add(lblNewLabel_2);
+
+		JLabel lblNewLabel_3 = new JLabel("Productos");
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_3.setForeground(lightGray);
+		lblNewLabel_3.setFont(txt);
+		tableTitles.add(lblNewLabel_3);
+
+		JLabel lblNewLabel_4 = new JLabel("Fecha de operación");
+		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_4.setForeground(lightGray);
+		lblNewLabel_4.setFont(txt);
+		tableTitles.add(lblNewLabel_4);
+
+		JLabel lblAcciones = new JLabel("Acciones");
+		lblAcciones.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAcciones.setForeground(lightGray);
+		lblAcciones.setFont(txt);
+		tableTitles.add(lblAcciones);
+
+		//TABLA
+		JPanel tablePanel = new JPanel();
+		tablePanel.setBounds(151, 184, 626, 322);
+		tablePanel.setLayout(new BorderLayout(0, 0));
+		tablePanel.setBackground(Color.white);
+		centro.add(tablePanel);
 	}
+
 
 }
