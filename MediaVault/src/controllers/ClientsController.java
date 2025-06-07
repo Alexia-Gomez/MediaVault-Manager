919fe7153815925ceb26326fd988d6e82b165a5d
Eduardo563 <erios_23@alu.uabcs.mx>
2025-06-07 01:24:48 -0700

    Actualizacion ClientsController

--- MediaVault/src/controllers/ClientsController.java
+++ MediaVault/src/controllers/ClientsController.java
@@ -1,15 +1,37 @@
 package controllers;
 
+import java.util.ArrayList;
+
+import models.Client;
+import models.ClientsModel;
 import views.ClientsView;
 
 public class ClientsController {
 	private ClientsView view;
+	private ClientsModel model;
 
 	public ClientsController() {
 		view = new ClientsView();
+		model = new ClientsModel();
 	}
 
 	public void clients() {
 		view.clients();
+	}
+	
+	public ArrayList<Client>  get() {
+		return model.getClients();
+	}
+	
+	public boolean add(Client client) {
+		return model.add(client);
+	}
+	
+	public boolean update(Client client) {
+		return model.update(client);
+	}
+	
+	public boolean delete(int client_id) {
+		return model.delete(client_id);
 	}
 }
